27109acbfc9a4902068b81f44db363ea6fe99eb7
Jefferson <jefferson.brandao@g.globo>
2023-05-22 18:50:36 -0300

    utilizando dados fakes no create question para testes

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Question,
@@ -5,14 +6,20 @@ import {
 } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    slug: Slug.create('qualquer-coisa'),
-    title: 'Qualquer coisa',
-    authorId: new UniqueEntityID(),
-    content: 'nova pergunta',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      slug: Slug.create('qualquer-coisa'),
+      title: faker.lorem.sentence(),
+      authorId: new UniqueEntityID(),
+      content: faker.lorem.paragraph(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
