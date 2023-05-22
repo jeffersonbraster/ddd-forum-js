4162d473b19321d341d235b9ffb1b5f84287ac9d
Jefferson <jefferson.brandao@g.globo>
2023-05-22 20:24:41 -0300

    criando caso de uso choose best answer

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -0,0 +1,50 @@
+import { AnswerRepository } from '../repositories/asnwer-repository'
+import { Question } from '../../enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
+
+interface ChooseQuestionBestAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+}
+
+interface ChooseQuestionBestAnswerUseCaseResponse {
+  question: Question
+}
+
+export class ChooseQuestionBestAnswerUseCase {
+  constructor(
+    private questionRepository: QuestionsRepository,
+    private answerRepository: AnswerRepository,
+  ) {}
+
+  async execute({
+    answerId,
+    authorId,
+  }: ChooseQuestionBestAnswerUseCaseRequest): Promise<ChooseQuestionBestAnswerUseCaseResponse> {
+    const answer = await this.answerRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found')
+    }
+
+    const question = await this.questionRepository.findById(
+      answer.questionId.toValue(),
+    )
+
+    if (!question) {
+      throw new Error('Question not found')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('You are not the author of this question')
+    }
+
+    question.bestAnswerId = answer.id
+
+    await this.questionRepository.save(question)
+
+    return {
+      question,
+    }
+  }
+}
