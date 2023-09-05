69b1db17598b6f01719dcb3d57fec3f91b7bb5de
Jefferson <jefferson.brandao@g.globo>
2023-09-04 21:15:25 -0300

    finalizando a classe de either e criando testes

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,22 +1,38 @@
 // Failure
-export class Left<L> {
+export class Left<L, R> {
   readonly value: any
 
   constructor(value: L) {
     this.value = value
   }
+
+  isRight(): this is Right<L, R> {
+    return false
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true
+  }
 }
 
 // Success
-export class Right<R> {
+export class Right<L, R> {
   readonly value: any
 
   constructor(value: R) {
     this.value = value
   }
+
+  isRight(): this is Right<L, R> {
+    return true
+  }
+
+  isLeft(): this is Left<L, R> {
+    return false
+  }
 }
 
-export type Either<L, R> = Left<L> | Right<R>
+export type Either<L, R> = Left<L, R> | Right<L, R>
 
 export const left = <L, R>(value: any): Either<L, R> => {
   return new Left(value)
